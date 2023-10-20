c103be8928867cd375af65669e0aefde6850dcd1
kelseybcrow <kelseybcrow@gmail.com>
2023-10-20 15:47:04 +0200

    finished form

--- app/Http/Controllers/AnimalCRUDController.php
+++ app/Http/Controllers/AnimalCRUDController.php
@@ -25,13 +25,14 @@ class AnimalCRUDController extends Controller
         $animal = new Animal();
         $animal->name = $request->input("name");
         $animal->owner_id = $request->input("owner_id");
-        // $animal->breed = $request->input("breed");
+        $animal->species = $request->input("species");
+        $animal->breed = $request->input("breed");
         $animal->age = $request->input("age");
-        // $animal->weight = $request->input("weight");
+        $animal->weight = $request->input("weight");
         $animal->save();
 
         session()->flash("success-message", 'Animal was successfully saved');
 
-        return redirect()->route('search');
+        return redirect()->route('animals');
     }
 }
--- resources/views/animals/form.blade.php
+++ resources/views/animals/form.blade.php
@@ -5,15 +5,34 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Add animal</title>
+    <style>
+        body {
+            margin-left: 1.5rem;
+        }
+        form {
+            display: flex;
+            flex-direction: column;
+            width: 50%;
+        }
+        h1 {
+            font-size: 2.5rem;
+        }
+        input, button, label {
+            font-size: 1.5rem;
+        }
+
+    </style>
 </head>
 <body>
     @include('components.messages')
+
+    <h1>Add your pet</h1>
 
     <form action="{{route('animals.store')}}" method="post">
     @csrf
 
     <br>
-    <label>Name:</label>
+    <label>Animal name:</label>
     <br>
     <input type="text" name="name" value="{{ old('name', $animal->name)}}">
 
@@ -23,9 +42,25 @@
     <input type="number" name="owner_id" value="{{ old('owner_id', $animal->owner ? $animal->owner->id : '')}}">
 
     <br>
+    <label>Species:</label>
+    <br>
+    <input type="text" name="species" value="{{ old('species', $animal->species)}}">
+
+    <br>
+    <label>Breed:</label>
+    <br>
+    <input type="text" name="breed" value="{{ old('breed', $animal->breed)}}">
+
+    <br>
     <label>Age:</label>
     <br>
     <input type="number" name="age" value="{{ old('age', $animal->age)}}">
+
+    <br>
+    <label>Weight:</label>
+    <br>
+    <input type="number" name="weight" value="{{ old('weight', $animal->weight)}}">
+    <br>
 
     <br>
     <button>Submit</button>
